846bd4a1d0b789cb3dffac7cd02cdabe1a3fcf3d
mohamedali252222 <mohamedali252222@gmail.com>
2026-06-01 14:25:44 +0300

    Add offer coupons cubit and home section

--- lib/logic/cubit/offer_coupons/offer_coupons_cubit.dart
+++ lib/logic/cubit/offer_coupons/offer_coupons_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
 
 import '../../../data/data_provider/remote_data_source.dart';
 import '../../../data/models/offer_coupon/offer_coupon_model.dart';
